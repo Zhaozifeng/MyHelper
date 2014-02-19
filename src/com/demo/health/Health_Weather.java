c0917bad451e6825d3708b2a6e57b5fda58431b7
Zhaozifeng <zhaozifeng222@163.com>
2014-02-19 16:15:23 +0800

    完成天气获取功能。

--- src/com/demo/health/Health_Weather.java
+++ src/com/demo/health/Health_Weather.java
@@ -7,6 +7,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.demo.myhelper.R;
@@ -14,6 +15,7 @@ import com.demo.myhelper.R;
 import android.app.Activity;
 import android.app.AlertDialog.Builder;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -29,9 +31,25 @@ public class Health_Weather extends Activity {
 	private ImageView imgBack;
 	private ImageView imgReflesh;
 	private TextView  tvTitle;	
-	public ProgressDialog loadDialog;
 	
+	public TextView   curTemp;
+	public TextView   curCity;
+	public TextView	  curWind;
+	public TextView	  curTips;
+	public TextView   curDescripe;
+	public TextView   Date;
+	public TextView   curWeather;
+	public TextView   XiChe;
+	public TextView   LvYou;
+	public TextView   ShuShi;
+	public TextView   YunDong;
+	public TextView   LiangShai;
+	public TextView   GanMao;
+	
+	public ProgressDialog loadDialog;	
 	public JSONObject WeatherJson;
+	
+	
 	
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -39,10 +57,30 @@ public class Health_Weather extends Activity {
 		setContentView(R.layout.activity_health_weather);
 		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.custom_title_layout);
 		initTitle();
-		WeatherConnectThread w = new WeatherConnectThread();
-		w.start();
+		alarmDialog();
 	}
-			
+		
+	
+	/*
+	 * 获取网络前提示
+	 */
+	public void alarmDialog(){
+		Builder builder = new Builder(Health_Weather.this);
+		builder
+		.setTitle("开发者提醒")
+		.setMessage("由于技术不成熟，您若是用2g网络将无法获取天气信息，请用3g或者WIFI链接网络，我们将进一步完善产品。")
+		.setPositiveButton("现在就连", new DialogInterface.OnClickListener() {			
+			public void onClick(DialogInterface dialog, int which) {
+				showProgressDialog("正在更新天气信息");
+				WeatherConnectThread w = new WeatherConnectThread();
+				w.start();
+			}
+		})
+		.setNegativeButton("更改网络", null);
+		builder.create().show();
+	}
+	
+	
 	/**
 	 * 初始化标题栏
 	 */	
@@ -59,12 +97,72 @@ public class Health_Weather extends Activity {
 		imgReflesh.setBackgroundDrawable(getResources().getDrawable(R.drawable.weather_reflesh));
 		imgReflesh.setOnClickListener(new OnClickListener(){
 			public void onClick(View v) {
-				showProgressDialog("正在更新天气信息");	
-				WeatherConnectThread w = new WeatherConnectThread();
-				w.start();
+				alarmDialog();
 			}			
-		});	
+		});			
+		curTemp    = (TextView)findViewById(R.id.weather_temperture_tv);
+		curCity    = (TextView)findViewById(R.id.weather_city_tv);
+		curWind	   = (TextView)findViewById(R.id.weather_wind_tv);
+		curTips	   = (TextView)findViewById(R.id.weather_tips_tv);
+		Date	   = (TextView)findViewById(R.id.weather_date_tv);
+		curDescripe= (TextView)findViewById(R.id.weather_descripe_tv);
+		XiChe      = (TextView)findViewById(R.id.weather_xc_tv);
+		LvYou	   = (TextView)findViewById(R.id.weather_tr_tv);
+		ShuShi	   = (TextView)findViewById(R.id.weather_co_tv);
+		YunDong    = (TextView)findViewById(R.id.weather_cl_tv);
+		LiangShai  = (TextView)findViewById(R.id.weather_ls_tv);
+		GanMao     = (TextView)findViewById(R.id.weather_ag_tv);
 	}
+	
+	
+	/**
+	 * 展示天气数据信息
+	 * @throws JSONException 
+	 */
+	public void initWeatherInfo(){
+		String temp 	= null;
+		String city 	= null;
+		String weather  = null;
+		String date 	= null;
+		String wind 	= null;
+		String xiche 	= null;
+		String lvyou 	= null;
+		String shushi	= null;
+		String yundong	= null;
+		String liangshai= null;
+		String ganmao 	= null;
+		String tips     = null;
+		try {
+			temp 	= WeatherJson.getString("temp1");
+			city 	= WeatherJson.getString("city");
+			weather = WeatherJson.getString("weather1");
+			date 	= WeatherJson.getString("date_y")+WeatherJson.getString("week");
+			wind 	= WeatherJson.getString("wind1")+"，"+WeatherJson.getString("fl1");
+			xiche	= "洗车 ："+WeatherJson.getString("index_xc");
+			lvyou	= "旅游 ："+WeatherJson.getString("index_tr");
+			shushi	= "舒适 ："+WeatherJson.getString("index_co");
+			yundong = "运动 ："+WeatherJson.getString("index_cl");
+			liangshai = "晾晒 ："+WeatherJson.getString("index_ls");
+			ganmao	= "感冒 ："+WeatherJson.getString("index_ag");
+			tips	= "温馨提示 ："+WeatherJson.getString("index_d");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		curTemp.setText(temp);
+		curCity.setText(city);
+		curDescripe.setText(weather);
+		Date.setText(date);
+		curWind.setText(wind);
+		XiChe.setText(xiche);
+		LvYou.setText(lvyou);
+		ShuShi.setText(shushi);
+		YunDong.setText(yundong);
+		LiangShai.setText(liangshai);
+		GanMao.setText(ganmao);
+		curTips.setText(tips);
+	}
+	
+	
 	
 
 	/*
@@ -97,7 +195,11 @@ public class Health_Weather extends Activity {
 			case 0:
 				loadDialog.dismiss();
 				showResultDialog("更新数据成功");
+				initWeatherInfo();
 				break;
+			case 1:
+				showResultDialog("获取网路数据出错，请检查网络");
+				break;				
 			default:break;			
 			}
 		}
@@ -110,7 +212,8 @@ public class Health_Weather extends Activity {
 				
 			} catch (Exception e) {
 				loadDialog.dismiss();
-				showResultDialog("获取网路数据出错，请检查网络");
+				//showResultDialog("获取网路数据出错，请检查网络");
+				handler.sendEmptyMessage(1);
 			}
 		}	
 	}
@@ -121,7 +224,7 @@ public class Health_Weather extends Activity {
 	 * @throws Exception 
 	 */
 	public void getJsonData() throws Exception{
-		String path = "http://m.weather.com.cn/data/101090507.html";
+		String path = "http://m.weather.com.cn/data/101280101.html";
 		URL url = new URL(path);
 		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
 		conn.setReadTimeout(8000);
